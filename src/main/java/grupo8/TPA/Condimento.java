8cc8dd6c6ae476f0fc107b1fcb20a980339805d7
Ivo Ursino <ursinoivo@gmail.com>
2015-05-02 14:21:13 -0300

    Agregadas las clases Usuario, Receta, Condimento e Ingrediente. Condimento e Ingrediente son exactamente iguales, tal vez deberiamos hacer una superclase. En las mismas, puse las calorias como atributo porque aun no sabemos como calcularlas, lo hice para que funcione por el momento. Creada la interfaz Temporada. Revisar calculo de caloriasTotales() en Receta (me quedo medio largo)

---
+++ src/main/java/grupo8/TPA/Condimento.java
@@ -0,0 +1,15 @@
+package grupo8.TPA;
+
+public class Condimento {
+	
+	String nombre;
+	double cantidad;
+	double calorias;
+	
+	
+	double calorias()
+	{
+		return calorias;
+	}
+
+}
---
+++ src/main/java/grupo8/TPA/Usuario.java
@@ -0,0 +1,18 @@
+package grupo8.TPA;
+
+public class Usuario {
+
+	private Double peso;
+	private Double altura;
+
+	public Usuario(Double unPeso, Double unaAltura) {
+		peso = unPeso;
+		altura = unaAltura;
+	}
+
+	public Double calcularIMC() {
+		return peso / Math.pow(altura, 2); // me dio paja buscar una funcion que
+											// sea el cuadrado de un numero
+	}
+
+}
