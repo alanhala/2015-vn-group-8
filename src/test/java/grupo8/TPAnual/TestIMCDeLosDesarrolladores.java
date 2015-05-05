2935a00e18d9bc161411f9c46392504cf3c3c265
Alan Halatian <alanhalatian@hotmail.com>
2015-05-05 02:01:38 -0300

    agregue el test de IMC de alan

--- src/test/java/grupo8/TPAnual/TestIMCDeLosDesarrolladores.java
+++ src/test/java/grupo8/TPAnual/TestIMCDeLosDesarrolladores.java
@@ -10,12 +10,14 @@ public class TestIMCDeLosDesarrolladores {
 	private Usuario felipe;
 	private Usuario crespo;
 	private Usuario ivo;
+	private Usuario alan;
 	
 	@Before
 	public void init() {
 		felipe = new Usuario(55.1, 1.69);
 		crespo = new Usuario(72.2,1.81);
 		ivo = new Usuario(65.2,1.78);
+		alan = new Usuario(90.00,1.80);
 	}
 	  
 	@Test
@@ -37,6 +39,13 @@ public class TestIMCDeLosDesarrolladores {
 		Double imcDeIvo = ivo.calcularIMC();
 		
 		assertEquals(imcDeIvo, (Double) 20.578, 0.001);
+	}
+	
+	@Test
+	public void IMCdeAlanDa27_77() {
+		Double imcDeAlan = alan.calcularIMC();
+		
+		assertEquals(imcDeAlan, (Double) 27.777, 0.001);
 	}
 
 }
