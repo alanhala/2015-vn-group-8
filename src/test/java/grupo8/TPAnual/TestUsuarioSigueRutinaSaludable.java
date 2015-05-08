fe7ee8bf60135addd06c6aee59eae9412073f489
Juan Manuel Crespo <juanmacrespo87@gmail.com>
2015-05-08 02:26:59 -0300

    corrijo un par de cosas que hice para el orto y subo TestUsuarioSigueRutinaSaludable

---
+++ src/test/java/grupo8/TPAnual/TestUsuarioSigueRutinaSaludable.java
@@ -0,0 +1,49 @@
+package grupo8.TPAnual;
+
+import static org.junit.Assert.*;
+import grupo8.TPAnual.exceptions.UsuarioInvalidoException;
+import grupo8.TPAnual.model.ComponenteDeReceta;
+import grupo8.TPAnual.model.Hipertenso;
+import grupo8.TPAnual.model.Receta;
+import grupo8.TPAnual.model.Temporada;
+import grupo8.TPAnual.model.Usuario;
+import grupo8.TPAnual.model.Vegano;
+import grupo8.TPAnual.model.Usuario.Rutina;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestUsuarioSigueRutinaSaludable {
+	private Usuario juan, oscar, pepe;
+	private Hipertenso hipertenso;
+	private Vegano vegano; 
+			
+	@Before
+	public void init() {
+		hipertenso = new Hipertenso();
+		vegano = new Vegano();
+		juan = new Usuario(72.2, 1.81, "Juan Manuel", "masculino", LocalDate.of(1994, 11, 14), Arrays.asList("sopa", "pasta"), Arrays.asList("polenta", "pollo"), Arrays.asList(), Arrays.asList(), Rutina.LEVE);
+		oscar = new Usuario(80.5, 1.80, "Oscar", "masculino", LocalDate.of(1994, 9, 24), Arrays.asList("queso", "pescado", "frutas"), Arrays.asList("polenta", "fideos"), Arrays.asList(vegano, hipertenso), Arrays.asList(), Rutina.INTENSIVO);
+		pepe = new Usuario(70.0,1.70, "Pepex", "masculino", LocalDate.of(1990, 4, 2), Arrays.asList("asado", "chivito"), Arrays.asList(), Arrays.asList(hipertenso), Arrays.asList(), Rutina.LEVE);
+	}
+
+
+	@Test
+	public void juanSigueRutinaSaludable() {
+		assertTrue(juan.sigueRutinaSaludable());
+	}
+	
+	@Test
+	public void oscarSigueRutinaSaludable() {
+		assertTrue(oscar.sigueRutinaSaludable());
+	}
+
+	@Test
+	public void pepeNoSigueRutinaSaludable() {
+		assertFalse(pepe.sigueRutinaSaludable());
+	}
+	
+}
--- src/test/java/grupo8/TPAnual/TestUsuarioValido.java
+++ src/test/java/grupo8/TPAnual/TestUsuarioValido.java
@@ -18,20 +18,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestUsuarioValido {
-	private Usuario juan;
-	private Usuario oscar;
+	private Usuario juan, oscar;
 	private Hipertenso hipertenso;
 	private Vegano vegano; 
-	private Receta receta;
-	private Temporada temporada; 
-	private ComponenteDeReceta ingrediente1, condimento1;
 			
 	@Before
 	public void init() {
 		hipertenso = new Hipertenso();
 		vegano = new Vegano();
-		receta = new Receta("nombre", Arrays.asList(ingrediente1), Arrays.asList(condimento1), "preparacion", "dificultad", temporada);
-		juan = new Usuario(72.2, 1.81, "Juan Manuel", "masculino", LocalDate.of(1994, 11, 14), Arrays.asList("sopa", "pasta"), Arrays.asList("polenta", "pollo"), Arrays.asList(hipertenso, vegano), Arrays.asList(receta), Rutina.LEVE);
+		juan = new Usuario(72.2, 1.81, "Juan Manuel", "masculino", LocalDate.of(1994, 11, 14), Arrays.asList("sopa", "pasta"), Arrays.asList("polenta", "pollo"), Arrays.asList(hipertenso, vegano), Arrays.asList(), Rutina.LEVE);
 		}
 
 
@@ -43,7 +38,7 @@ public class TestUsuarioValido {
 	@Test
 	public void creacionDeOscarTiraExcepcion() {
 		try {
-		oscar = new Usuario(72.2, 1.81, "Oscar", "masculino", LocalDate.of(1994, 11, 14), Arrays.asList("carne", "pollo"), Arrays.asList("queso", "pescado"), Arrays.asList(vegano), Arrays.asList(receta), Rutina.LEVE);
+		oscar = new Usuario(80.5, 1.80, "Oscar", "masculino", LocalDate.of(1994, 9, 24), Arrays.asList("carne", "pollo"), Arrays.asList("queso", "pescado"), Arrays.asList(vegano), Arrays.asList(), Rutina.LEVE);
 		} catch (UsuarioInvalidoException e) {
 			return;
 		}
