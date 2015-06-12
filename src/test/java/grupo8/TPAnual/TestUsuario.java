bc0829595e8476ae3bbfd94a818848047f59199e
Ivo Ursino <ursinoivo@gmail.com>
2015-06-12 01:04:09 -0300

    Acomodo tests de usuario

--- src/test/java/grupo8/TPAnual/TestUsuario.java
+++ src/test/java/grupo8/TPAnual/TestUsuario.java
@@ -32,12 +32,12 @@ public class TestUsuario {
 
 	@Before
 	public void init() {
-		
-		//Inicializacion de usuarios
-		
+
+		// Inicializacion de usuarios
+
 		hipertenso = new Hipertenso();
 		vegano = new Vegano();
-		
+
 		juan = new Usuario(72.2, 1.81, "Juan Manuel", "masculino",
 				LocalDate.of(1994, 11, 14), Arrays.asList("sopa", "pasta"),
 				Arrays.asList("polenta", "pollo"), Arrays.asList(),
@@ -46,67 +46,71 @@ public class TestUsuario {
 		oscar = new Usuario(80.5, 1.80, "Oscar", "masculino", LocalDate.of(
 				1994, 9, 24), Arrays.asList("queso", "pescado", "frutas"),
 				Arrays.asList("polenta", "fideos"), Arrays.asList(vegano,
-						hipertenso), Arrays.asList(), Rutina.INTENSIVO, Arrays.asList());
+						hipertenso), Arrays.asList(), Rutina.INTENSIVO,
+				Arrays.asList());
 
 		pepe = new Usuario(70.0, 1.70, "Pepex", "masculino", LocalDate.of(1990,
 				4, 2), Arrays.asList("asado", "chivito"), Arrays.asList(),
-				Arrays.asList(hipertenso), Arrays.asList(), Rutina.LEVE, Arrays.asList());
-		
+				Arrays.asList(hipertenso), Arrays.asList(), Rutina.LEVE,
+				Arrays.asList());
+
 		osqui = new Usuario(80.0, 1.90, "Juan Manuel Oscar", "masculino",
 				LocalDate.of(1985, 10, 11), Arrays.asList(), Arrays.asList(),
-				Arrays.asList(vegano, hipertenso, diabetico), Arrays.asList(), 
-				Rutina.MEDIANO, Arrays.asList()); 
-		
-		//Inicializacion de recetas
-		
-		colitaDeCuadril = new ComponenteDeReceta("Colita de cuadril", 500.0, 1000.0);
+				Arrays.asList(vegano, hipertenso, diabetico), Arrays.asList(),
+				Rutina.MEDIANO, Arrays.asList());
+
+		// Inicializacion de recetas
+
+		colitaDeCuadril = new ComponenteDeReceta("Colita de cuadril", 500.0,
+				1000.0);
 		papa = new ComponenteDeReceta("Papa", 200.0, 350.0);
 		ingredientes.add(colitaDeCuadril);
 		ingredientes.add(papa);
-		
-		carneAlHornoConPapas = new Receta("Carne al horno con papas", ingredientes, condimentos, 1350.0,juan,true);
+
+		carneAlHornoConPapas = new Receta("Carne al horno con papas",
+				ingredientes, condimentos, 1350.0, juan, true);
 	}
-	
-	@Test 
-	public void juanTieneNombreValido()	{
+
+	@Test
+	public void juanTieneNombreValido() {
 		juan.tieneNombreValido();
 	}
-	
+
 	@Test
 	public void juanTieneFechaDeNacimientoValida() {
 		juan.tieneFechaNacimientoValida();
 	}
-	
+
 	@Test
 	public void juanTieneCamposObligatorios() {
 		juan.tieneCamposObligatorios();
 	}
-	
+
 	@Test
 	public void juanTienePreferenciasAlimenticias() {
 		assertTrue(juan.tienePreferenciasAlimenticias());
 	}
-	
+
 	@Test
 	public void juanTieneSexo() { // XD
 		assertTrue(juan.tieneSexo());
 	}
-	
+
 	@Test
 	public void aOscarLeGustaElQueso() {
 		assertTrue(oscar.leGusta("queso"));
 	}
-	
+
 	@Test
 	public void aPepeNoLeGustaElChori() {
 		assertFalse(pepe.leGusta("chori"));
 	}
-	
+
 	@Test
 	public void juanEsValido() {
 		juan.esValido();
 	}
-	
+
 	@Test
 	public void juanSigueRutinaSaludable() {
 		assertTrue(juan.sigueRutinaSaludable());
@@ -121,74 +125,78 @@ public class TestUsuario {
 	public void pepeNoSigueRutinaSaludable() {
 		assertFalse(pepe.sigueRutinaSaludable());
 	}
-	
+
 	@Test
 	public void aJuanNoLeDisgustaLaCarneAlHornoConPapas() {
 		assertFalse(juan.leDisgusta(carneAlHornoConPapas));
 	}
-	
+
 	@Test
 	public void aPepeSeLePuedeSugerirLaCarneAlHornoConPapas() {
 		assertTrue(pepe.seLePuedeSugerir(carneAlHornoConPapas));
 	}
-	
+
 	@Test(expected = VeganoInvalidoException.class)
 	public void oscarEsVeganoInvalidoPorComerCarneYPollo() {
 		oscar = new Usuario(80.5, 1.80, "Oscar", "masculino", LocalDate.of(
 				1994, 9, 24), Arrays.asList("carne", "pollo"), Arrays.asList(
 				"queso", "pescado"), Arrays.asList(vegano), Arrays.asList(),
 				Rutina.LEVE, null);
-		
+
 		oscar.esValido();
 	}
-	
-//	@Test
-//	public void juanPepeYOscarEstanEnSolicitudesPendientes() {
-//		assertTrue(GestorPerfiles.solicitudesPendientesDeUsuarios.containsAll(Arrays.asList(juan, pepe, oscar)));
-//	}
-//	
-//	@Test
-//	public void juanEsAceptadoYEntraEnElRepoUsuarios() {
-//		GestorPerfiles.aceptarPerfil(juan);
-//		
-//		assertFalse(GestorPerfiles.solicitudesPendientesDeUsuarios.contains(juan));
-//		assertTrue(RepoUsuarios.usuarios.contains(juan));
-//	}
-//	
-//	@Test
-//	public void osquiTieneCondicionesDeOscar() {
-//		GestorPerfiles.aceptarPerfil(oscar);
-//		GestorPerfiles.aceptarPerfil(osqui);
-//		
-//		assertTrue(osqui.tieneLasCondicionesDe(oscar));
-//	}
-//	
-//	@Test
-//	public void buscarUsuarioConGetEnRepoUsuarios() {
-//		GestorPerfiles.aceptarPerfil(juan);
-//		GestorPerfiles.aceptarPerfil(oscar);
-//		GestorPerfiles.aceptarPerfil(pepe);
-//		
-//		Usuario usuarioFiltrado = RepoUsuarios.get(juan);
-//		
-//		assertTrue(usuarioFiltrado.equals(juan));
-//		
-//	}
-//	
-//	@Test
-//	public void filtrarUsuariosConNombreYCondiciones() {
-//		GestorPerfiles.aceptarPerfil(juan);
-//		GestorPerfiles.aceptarPerfil(oscar);
-//		GestorPerfiles.aceptarPerfil(pepe);
-//		GestorPerfiles.aceptarPerfil(osqui);
-//		
-//		List<Usuario> usuariosFiltrados = RepoUsuarios.list(oscar);
-//		
-//		assertTrue(usuariosFiltrados.contains(oscar));
-//		assertTrue(usuariosFiltrados.contains(osqui));
-//		assertFalse(usuariosFiltrados.contains(pepe));
-//		assertFalse(usuariosFiltrados.contains(juan));
-//
-//	}
+
+	@Test
+	public void juanPepeYOscarEstanEnSolicitudesPendientes() {
+		assertTrue(RepoUsuarios.solicitudesPendientesDeUsuarios
+				.containsAll(Arrays.asList(juan, pepe, oscar)));
+	}
+
+	@Test
+	public void juanEsAceptadoYEntraEnElRepoUsuarios() {
+		RepoUsuarios.aceptarPerfil(juan);
+
+		assertTrue(RepoUsuarios.usuarios.contains(juan));
+	}
+
+	@Test
+	public void juanEsAceptadoYEsEliminadoDeLasSolicitudesPendientes() {
+		RepoUsuarios.aceptarPerfil(juan);
+		assertFalse(RepoUsuarios.solicitudesPendientesDeUsuarios.contains(juan));
+	}
+
+	@Test
+	public void osquiTieneCondicionesDeOscar() {
+		RepoUsuarios.aceptarPerfil(oscar);
+		RepoUsuarios.aceptarPerfil(osqui);
+
+		assertTrue(osqui.tieneLasCondicionesDe(oscar));
+	}
+
+	@Test
+	public void buscarUsuarioConGetEnRepoUsuarios() {
+		RepoUsuarios.aceptarPerfil(juan);
+		RepoUsuarios.aceptarPerfil(oscar);
+		RepoUsuarios.aceptarPerfil(pepe);
+
+		Usuario usuarioFiltrado = RepoUsuarios.get(juan);
+
+		assertTrue(usuarioFiltrado.equals(juan));
+
+	}
+
+	@Test
+	public void filtrarUsuariosConNombreYCondiciones() {
+		RepoUsuarios.aceptarPerfil(juan);
+		RepoUsuarios.aceptarPerfil(oscar);
+		RepoUsuarios.aceptarPerfil(pepe);
+		RepoUsuarios.aceptarPerfil(osqui);
+
+		List<Usuario> usuariosFiltrados = RepoUsuarios.list(oscar);
+
+		assertTrue(usuariosFiltrados.containsAll(Arrays.asList(oscar, osqui))
+				&& !usuariosFiltrados.contains(pepe) && !usuariosFiltrados.contains(juan));
+
+	}
 
 }
