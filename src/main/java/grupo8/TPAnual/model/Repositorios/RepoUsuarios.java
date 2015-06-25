65b0378095fcaa41c4c6feb403d38df78113b27a
Alan Halatian <alanhalatian@hotmail.com>
2015-06-24 22:55:35 -0300

    no es mas singleton y ademas al rechazar un perfil ahora guarda el usuario que fue rechazado junto con el motivo dentro de un hash

--- src/main/java/grupo8/TPAnual/model/Repositorios/RepoUsuarios.java
+++ src/main/java/grupo8/TPAnual/model/Repositorios/RepoUsuarios.java
@@ -4,85 +4,84 @@ import grupo8.TPAnual.exceptions.UsuarioNoEstaEnElRepositorioException;
 import grupo8.TPAnual.exceptions.UsuarioNoEstaEnListaDePendientesException;
 import grupo8.TPAnual.model.Dominio.Usuario;
 import grupo8.TPAnual.model.Monitores.GestorDeConsultas;
-import grupo8.TPAnual.model.Monitores.Notificador;
-import grupo8.TPAnual.model.Monitores.OtroNotificador;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class RepoUsuarios {
-	
-	private static RepoUsuarios instance;
-	private  List<Usuario> usuarios = new ArrayList<Usuario>();
-	private  List<Usuario> solicitudesPendientesDeUsuarios = new ArrayList<Usuario>();
-	
-	private Notificador gestorDeConsultas;
-	
-	
-	
-	public RepoUsuarios(Notificador gestorDeConsultas) {
+
+	private List<Usuario> usuarios = new ArrayList<Usuario>();
+	private List<Usuario> solicitudesPendientesDeUsuarios = new ArrayList<Usuario>();
+	private HashMap<Usuario, String> solicitudesRechazadas;
+	private GestorDeConsultas gestorDeConsultas;
+
+	public RepoUsuarios(GestorDeConsultas gestorDeConsultas) {
 		this.gestorDeConsultas = gestorDeConsultas;
 	}
 
-	public RepoUsuarios(){
-		gestorDeConsultas = new OtroNotificador();
+	public RepoUsuarios() {
+		this.gestorDeConsultas = new GestorDeConsultas();
 	}
-	
+
 	public void add(Usuario usuario) {
 		usuario.setGestorDeConsultas(this.gestorDeConsultas);
-		usuarios.add(usuario);		
-	}
+		usuarios.add(usuario);
+	}// TODO preguntar si no hay que validar que sea un usuario que esta
+		// pendiente
 
 	public void remove(Usuario usuario) {
 		usuarios.remove(usuario);
 	}
-	
+
 	public void update(Usuario usuario) {
-		try 
-		{
+		try {
 			this.get(usuario);
-			
-		}catch(NoSuchElementException exception)
-		{	
-			throw new UsuarioNoEstaEnElRepositorioException("El usuario no se encuentra en el repositorio");
+
+		} catch (NoSuchElementException exception) {
+			throw new UsuarioNoEstaEnElRepositorioException(
+					"El usuario no se encuentra en el repositorio");
 		}
-		
+
 		usuarios.remove(this.get(usuario));
 		usuarios.add(usuario);
 	}
-	
+
 	public Usuario get(Usuario usuario) {
-		return usuarios.stream().filter(u -> (u.tieneMismoNombreQue(usuario))).findFirst().get();
+		return usuarios.stream().filter(u -> (u.tieneMismoNombreQue(usuario)))
+				.findFirst().get();
 	}
-	
+
 	public List<Usuario> list(Usuario usuario) {
-		
-		Stream<Usuario> usuariosFiltrados = usuarios.stream().
-				filter(u -> u.nombreContieneNombreDe(usuario) && u.tieneLasCondicionesDe(usuario));
-		
+
+		Stream<Usuario> usuariosFiltrados = usuarios.stream().filter(
+				u -> u.nombreContieneNombreDe(usuario)
+						&& u.tieneLasCondicionesDe(usuario));
+
 		return usuariosFiltrados.collect(Collectors.toList());
 	}
-	
-	public void seCreoNuevoPerfil(Usuario usuario) {
+
+	public void agregarAPendienteDeAprobacion(Usuario usuario) {
 		solicitudesPendientesDeUsuarios.add(usuario);
 	}
-	
+
 	public void aceptarPerfil(Usuario usuario) {
 		chequearYRemoverUsuario(usuario);
 		usuarios.add(usuario);
 	}
-	
-	public String rechazarPerfil(Usuario usuario, String motivo) {
+
+	public void rechazarPerfil(Usuario usuario, String motivo) {
 		chequearYRemoverUsuario(usuario);
-		return motivo;
+		solicitudesRechazadas.put(usuario, motivo);
 	}
 
 	private void chequearYRemoverUsuario(Usuario usuario) {
 		if (!solicitudesPendientesDeUsuarios.contains(usuario)) {
-			throw new UsuarioNoEstaEnListaDePendientesException("El usuario ingresado no se encuentra en la lista de solicitudes pendientes");
+			throw new UsuarioNoEstaEnListaDePendientesException(
+					"El usuario ingresado no se encuentra en la lista de solicitudes pendientes");
 		}
 		solicitudesPendientesDeUsuarios.remove(usuario);
 	}
@@ -94,6 +93,5 @@ public class RepoUsuarios {
 	public List<Usuario> getUsuarios() {
 		return usuarios;
 	}
-	
-	
+
 }
