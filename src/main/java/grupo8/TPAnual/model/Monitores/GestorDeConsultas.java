323e24cf55df9b9aa0828e1e19aa9e25287db017
Alan Halatian <alanhalatian@hotmail.com>
2015-06-24 22:54:25 -0300

    el gestor no es mas un singleton

--- src/main/java/grupo8/TPAnual/model/Monitores/GestorDeConsultas.java
+++ src/main/java/grupo8/TPAnual/model/Monitores/GestorDeConsultas.java
@@ -7,32 +7,21 @@ import grupo8.TPAnual.model.Dominio.Usuario;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GestorDeConsultas implements Notificador {
-	private static GestorDeConsultas instance;
+public class GestorDeConsultas {
 	private List<Monitor> monitores;
-	
-	public GestorDeConsultas(){
+
+	public GestorDeConsultas() {
 		monitores = new ArrayList<Monitor>();
 	}
-	
-	public static GestorDeConsultas getInstance(){
-		if (instance == null){
-			instance = new GestorDeConsultas();
-		}
-		
-		return instance;
-	}
-	
+
 	public void agregarMonitor(Monitor unMonitor) {
 		monitores.add(unMonitor);
 	}
-	
+
 	public void removerMonitor(Monitor unMonitor) {
 		monitores.remove(unMonitor);
 	}
-	
 
-	@Override
 	public void notificar(Usuario usuario, List<Receta> consulta) {
 		monitores.forEach(monitor -> monitor.actualizar(usuario, consulta));
 	}
